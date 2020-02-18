a0abefb535b619f4b53838b38124ff4a0c8312e8
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2020-02-17 21:55:39 -0500

    m

--- matrix.cpp
+++ matrix.cpp
@@ -2,7 +2,7 @@
 #include "matrix.hpp"
 
 template<typename T>
-T MATRIX<T>::get(int i, int j){
+T MATRIX<T>::get(int i, int j) const{
     matrix += (columns*i + j);
     T val = *matrix;
     matrix -= (columns*i + j);
@@ -10,10 +10,25 @@ T MATRIX<T>::get(int i, int j){
 }
 
 template<typename T>
-void MATRIX<T>::set(int i, int j, T val){
+T MATRIX<T>::get(int i) const{
+    matrix += i;
+    T val = *matrix;
+    matrix -= i;
+    return val;
+}
+
+template<typename T>
+void MATRIX<T>::set(int i, int j, T val) const{
     matrix += (columns*i + j);
     *matrix = val;
     matrix -= (columns*i + j);
+}
+
+template<typename T>
+void MATRIX<T>::set(int i, T val) const{
+    matrix += i;
+    *matrix = val;
+    matrix -= i;
 }
 
 template<typename T>
@@ -25,21 +40,48 @@ MATRIX<T>::MATRIX(int rows, int columns){
 
 template<typename T>
 MATRIX<T>::~MATRIX(){
-    free(this->matrix);
+    free(matrix);
+}
+
+template<typename T>
+int MATRIX<T>::getRows() const{ return rows; }
+
+template<typename T>
+int MATRIX<T>::getColumns() const{ return columns; }
+
+template<typename T>
+int MATRIX<T>::getLength() const{ return (rows * columns); }
+
+template<typename T>
+template<typename U>
+MATRIX<T> MATRIX<T>::operator+(MATRIX<U> m) const {
+    MATRIX<T>* mat = new MATRIX(rows, columns);
+    for(int i = 0; i < this->getLength(); i++){
+        mat->set(i, ((T)(m.get(i)) + this->get(i)));
+    }
+    return *mat;
 }
 
 int main(int argv, char** argc){
-    MATRIX<int>* mat = new MATRIX<int>(3, 5);
+    MATRIX<int>* mat1 = new MATRIX<int>(3, 5);
+    MATRIX<double>* mat2 = new MATRIX<double>(3, 5);
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 5; j++){
-            mat->set(i, j, i*j);
+            mat1->set(i, j, 1);
+            mat2->set(i, j, 2.0);
         }
     }
 
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 5; j++){
-            printf("%-5d", mat->get(i, j));
+    MATRIX<double> mat3 = (*mat2)+(*mat1);
+
+    for(int i = 0; i < mat3.getRows(); i++){
+        for(int j = 0; j < mat3.getColumns(); j++){
+            printf("%-5.3f", mat3.get(i, j));
         }
         std::cout << std::endl;
     }
+
+    delete mat1;
+    delete mat2;
+    delete &mat3;
 }
--- matrix.hpp
+++ matrix.hpp
@@ -2,15 +2,22 @@
 
 template<typename T>
 class MATRIX{
-    
-    private:
-        int rows;
-        int columns;
-        T* matrix;
-    
-    public:
-        T get(int, int);
-        void set(int, int, T);
-        MATRIX(int, int);
-        ~MATRIX();
+	
+	private:
+		int rows;
+		int columns;
+		mutable T* matrix;
+	
+	public:
+		T get(int, int) const;      // Gets a value from the matrix
+		T get(int) const;      		// Gets a value from the matrix
+		void set(int, int, T) const;      // Sets a value in the matrix
+		void set(int, T) const;      		// Sets a value in the matrix
+		int getRows() const;     	// Returns the number of rows
+		int getColumns() const;  	// Returns the number of column
+		int getLength() const;   	// Retruns the total length of the matrix (rows x columns)
+		template<typename U>
+		MATRIX<T> operator+(MATRIX<U>) const;   // Add matrices together
+		MATRIX(int, int);        	// Constructor (takes in length and width of matrix)
+		~MATRIX();               	// Destructor (frees the memory)
 };
