7f8adfe0bf2d2745a154ad859cee14ad864e7782
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2020-02-20 20:16:27 -0500

    m

--- matrix.hpp
+++ matrix.hpp
@@ -1,7 +1,19 @@
 #pragma once
 #include <stdlib.h>
+#include <iostream>
+#include <exception>
 
-template<typename T>
+#define TYPE_T template<typename T>
+#define TYPE_U template<typename U>
+
+class DimensionException : public std::exception{
+	public:
+		const char* what() const throw(){
+			return "The dimensions of the matrices are not compatible with the specified operation";
+		}
+};
+
+TYPE_T
 class MATRIX{
 	
 	private:
@@ -10,9 +22,11 @@ class MATRIX{
 		int length;                 // number of rows x number of columns = total length of the matrix
 		T* matrix;                  // Pointer to the memory location where the actual matrix array is located
 		bool flag = 1;              // Whether matrix needs to be deleted by the destructor or not (determied by which constructor is being used)
-		int iterator = 0;           // Iterates throught the matrix. Resets once it reaches the length of the matrix
 	
 	public:
+		MATRIX(int, int);        	// Constructor (takes in length and width of matrix)
+		MATRIX(int, int, T*);       // Constructor (takes in length, width, and a pointer to an already allocated space of memory. Could be used to create constant matrices)
+		~MATRIX();               	// Destructor (frees the memory)
 		T get(int, int) const;      // Gets a value from the matrix from a position relative to the rows and columns
 		T get(int) const;      		// Gets a value from the matrix from a position relative to the length of the matrix (rows x columns)
 		void set(int, int, T);      // Sets a value in the matrix at a specific location relative to rows and columns
@@ -20,24 +34,27 @@ class MATRIX{
 		int getRows() const;     	// Returns the number of rows
 		int getColumns() const;  	// Returns the number of column
 		int getLength() const;   	// Returns the total length of the matrix (rows x columns)
-		template<typename U>
-		MATRIX<T> operator+(MATRIX<U>&) const;   // Add matrices together
-		MATRIX(int, int);        	// Constructor (takes in length and width of matrix)
-		MATRIX(int, int, T*);       //
-		~MATRIX();               	// Destructor (frees the memory)
+		// Operators ---------
+		TYPE_U MATRIX<T> operator+(const MATRIX<U>&) const;   // Add matrices
+		TYPE_U MATRIX<T> operator-(const MATRIX<U>&) const;   // Subtract matrices
+		TYPE_U MATRIX<T> operator*(const MATRIX<U>&) const;   // Multiply matrices
+		TYPE_U MATRIX<T> operator/(const MATRIX<U>&) const;   // Divide Matrices
+		TYPE_U MATRIX<T> operator*(U) const;            // Scalar Multiplier
+		TYPE_U MATRIX<T> operator/(U) const;            // Scalar Divisor
 };
 
 // =========== Matrix Class =================
 
-// Matic Constructor ====
-template<typename T>
+// Matix Constructor ====
+TYPE_T
 MATRIX<T>::MATRIX(int rows, int columns) : 
 	rows(rows), 
 	columns(columns),
 	length(rows*columns)
 { this->matrix = new T[length]; }
 
-template<typename T>
+// Matrix Constructor ====
+TYPE_T
 MATRIX<T>::MATRIX(int rows, int columns, T* matrixPointer) :
 	rows(rows), 
 	columns(columns),
@@ -47,57 +64,106 @@ MATRIX<T>::MATRIX(int rows, int columns, T* matrixPointer) :
 {}
 
 // Matrix Destrctor ======
-template<typename T>
+TYPE_T
 MATRIX<T>::~MATRIX(){
 	if(flag){
     	delete[] matrix;
+		std::cout << "DESTROY" << std::endl;
 	}
 }
 
 // Get =====
-template<typename T>
+TYPE_T
 T MATRIX<T>::get(int i, int j) const{
     return matrix[(columns*i)+j];
 }
 
 // Get =====
-template<typename T>
+TYPE_T
 T MATRIX<T>::get(int i) const{
     return matrix[i];
 }
 
 // Set =====
-template<typename T>
+TYPE_T
 void MATRIX<T>::set(int i, int j, T val) {
     matrix[(columns*i)+j] = val;
 }
 
 // Set ====
-template<typename T>
+TYPE_T
 void MATRIX<T>::set(int i, T val) {
     matrix[i] = val;
 }
 
 // Get Rows ======
-template<typename T>
+TYPE_T
 int MATRIX<T>::getRows() const{ return rows; }
 
 // Get Columns ========
-template<typename T>
+TYPE_T
 int MATRIX<T>::getColumns() const{ return columns; }
 
 // Get Length ========
-template<typename T>
+TYPE_T
 int MATRIX<T>::getLength() const{ return length; }
 
 // Operator+ =========
 
-template<typename T>
-template<typename U>
-MATRIX<T> MATRIX<T>::operator+(MATRIX<U>& m) const {      // Argument needs to be a reference, or else local copy will affect the input matrix
-    MATRIX<T> mat(rows, columns);
+TYPE_T TYPE_U
+MATRIX<T> MATRIX<T>::operator+(const MATRIX<U>& m) const {      // Argument needs to be a reference, or else local copy will affect the input matrix
+    if(m.getRows() != rows || m.getColumns() != columns){
+		throw DimensionException();
+	}
+	MATRIX<T> mat(rows, columns);
     for(int i = 0; i < getLength(); i++){
         mat.set(i, (((T)m.get(i)) + this->get(i)));
     }
     return mat;
+}
+
+// Operator- ==========
+
+TYPE_T TYPE_U
+MATRIX<T> MATRIX<T>::operator-(const MATRIX<U>& m) const {      // Argument needs to be a reference, or else local copy will affect the input matrix
+    if(m.getRows() != rows || m.getColumns() != columns){
+		throw DimensionException();
+	}
+	MATRIX<T> mat(rows, columns);
+    for(int i = 0; i < getLength(); i++){
+        mat.set(i, (((T)m.get(i)) - this->get(i)));
+    }
+    return mat;
+}
+
+// Operator* =========
+
+TYPE_T TYPE_U
+MATRIX<T> MATRIX<T>::operator*(const MATRIX<U>& m) const {
+	if(columns != m.getRows()){
+		throw DimensionException();
+	}
+	MATRIX<T> mat(rows, m.getColumns());
+	T temp = (T)0;
+	for(int i = 0; i < rows; i++){
+		for(int j = 0; j < m.getColumns(); j++){
+			for(int k = 0; k < columns; k++){
+				temp += (this->get(i, k))  *  ((T)m.get(k, j));
+			}
+			mat.set(i, j, temp);
+			temp = (T)0;
+		}
+	}
+	return mat;
+}
+
+// Operator* scalar =======
+
+TYPE_T TYPE_U
+MATRIX<T> MATRIX<T>::operator*(U s) const {
+	MATRIX<T> mat(rows, columns);
+	for(int i = 0; i < length; i++){
+		mat.set(i, get(i)*(T)s);
+	}
+
 }
--- test.cpp
+++ test.cpp
@@ -1,45 +1,41 @@
 #include <iostream>
 #include "matrix.hpp"
 
-int main(int argv, char** argc){
-
-	MATRIX<int> mat1(3, 5);
-	MATRIX<int> mat2(3, 5);
-	for(int i = 0; i < 3; i++){
-		for(int j = 0; j < 5; j++){
-			mat1.set(i, j, 5);
-			mat2.set(i, j, i*j);
-		}
-	}
-
-	MATRIX<int> mat3 = mat1+mat2;
-
-	std::cout << "Matrix 1:" << std::endl;
-	for(int i = 0; i < 3; i++){
-		for(int j = 0; j < 5; j++){
-			printf("%-5d", mat1.get(i, j));
+void printMatrix(const MATRIX<int>& m){
+	for(int i = 0; i < m.getRows(); i++){
+		for(int j = 0; j < m.getColumns(); j++){
+			printf("%-6d", m.get(i, j));
 		}
 		std::cout << std::endl;
 	}
-	
+}
+
+int main(int argv, char** argc){
+
+	MATRIX<int> mat1(3, 4);
+	MATRIX<int> mat2(4, 3);
+	for(int i = 0; i < mat1.getRows(); i++){
+		for(int j = 0; j < mat1.getColumns(); j++){
+			mat1.set(i, j, i+j);
+		}
+	}
+	for(int i = 0; i < mat2.getRows(); i++){
+		for(int j = 0; j < mat2.getColumns(); j++){
+			mat2.set(i, j, 5);
+		}
+	}
+
+	MATRIX<int> mat3 = mat1*mat2;
+
+	std::cout << "Matrix 1:" << std::endl;
+	printMatrix(mat1);
 	std::cout << std::endl;
 
 	std::cout << "Matrix 2:" << std::endl;
-	for(int i = 0; i < 3; i++){
-		for(int j = 0; j < 5; j++){
-			printf("%-5d", mat2.get(i, j));
-		}
-		std::cout << std::endl;
-	}
-	
+	printMatrix(mat2);
 	std::cout << std::endl;
 
-	std::cout << "Matrix 1 + Matrix 2:" << std::endl;
-	for(int i = 0; i < 3; i++){
-		for(int j = 0; j < 5; j++){
-			printf("%-5d", mat3.get(i, j));
-		}
-		std::cout << std::endl;
-	}
+	std::cout << "Matrix 1 * Matrix 2:" << std::endl;
+	printMatrix(mat3);
 
 }
