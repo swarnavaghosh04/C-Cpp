8debb9d0a5a79d306ca00993ab2c483d5945b0d6
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2020-02-26 22:16:21 -0500

    m

--- matrix.hpp
+++ matrix.hpp
@@ -13,11 +13,12 @@ MATRIX is a highly optimized stand-alone class that provides basic matrix operat
 */
 
 #pragma once
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <iostream>
 #include <exception>
 
 #define TYPE_T template<typename T>
+#define TYPE_U template<typename U>
 #define TYPE_AB template<typename A, typename B>
 
 class DimensionException : public std::exception{
@@ -35,18 +36,21 @@ class MATRIX{
 		int columns;                // number of columns in the matrix
 		int length;                 // number of rows x number of columns = total length of the matrix
 		T* matrix;                  // Pointer to the memory location where the actual matrix array is located
-		
+		typedef T (*FillFunction)(const int&, const int&);
 	public:
 		MATRIX(const int&, const int&);        	         // Constructor (takes in length and width of matrix)
 		MATRIX(const int&, const int&, T* const);        // Constructor (takes in length, width, and a pointer to an already allocated space of memory. Could be used to create constant matrices)
+		MATRIX(const MATRIX<T>&);                        // Copy Constructor (allocates new memory and copies matrix)
 		~MATRIX();               	                     // Destructor (frees the memory)
 		int getRows() const;     	        // Returns the number of rows
 		int getColumns() const;  	        // Returns the number of column
 		int getLength() const;   	        // Returns the total length of the matrix (rows x columns)
 		const T* getMatrix() const;         // Returns the location of the matrix
-		void fillMatrix(T (*)(int,int));    // Fills The matrix with 
+		void fillMatrix(FillFunction);          // Fills The matrix with a pattern based off of position
 		// Operators ---------
 		T* operator[](unsigned int i) const;                                       // Access numbers in the matrix
+		TYPE_U void operator=(const MATRIX<U>&);
+		TYPE_U void operator=(const MATRIX<U>&&);
 		TYPE_AB friend MATRIX<A> operator+(const MATRIX<A>&, const MATRIX<B>&);    // Add matrices
 		TYPE_AB friend MATRIX<A> operator-(const MATRIX<A>&, const MATRIX<B>&);    // Subtract matrices
 		TYPE_AB friend MATRIX<A> operator*(const MATRIX<A>&, const MATRIX<B>&);    // Multiply matrices
@@ -63,7 +67,7 @@ MATRIX<T>::MATRIX(const int& rows, const int& columns) :
 	rows(rows), 
 	columns(columns),
 	length(rows*columns)
-{ this->matrix = new T[length]; }
+{ matrix = new T[length]; }
 
 // Matrix Constructor (rows, columns, pointer to 1D heap allocated memory) ======
 TYPE_T
@@ -73,6 +77,17 @@ MATRIX<T>::MATRIX(const int& rows, const int& columns, T* const matrixPointer) :
 	length(rows*columns),
 	matrix(matrixPointer)
 {}
+
+// Copy Constructor (allocates new memory and copies matrix) ====================
+TYPE_T
+MATRIX<T>::MATRIX(const MATRIX<T>& mat) : 
+	rows(mat.rows),
+	columns(mat.columns),
+	length(mat.length)
+{
+	matrix = new T[length];
+	for(int i = 0; i< length; i++) matrix[i] = mat.matrix[i];
+}
 
 // Matrix Destrctor ======
 TYPE_T
@@ -93,13 +108,37 @@ int MATRIX<T>::getColumns() const{ return columns; }
 TYPE_T
 int MATRIX<T>::getLength() const{ return length; }
 
-// Get Matrix
+// Get Matrix ==========
 TYPE_T
 const T* MATRIX<T>::getMatrix() const{ return matrix; }
 
-// Operator- =========
+// Fill matrix ==========
+TYPE_T
+void MATRIX<T>::fillMatrix(FillFunction func){
+	for(int i = 0; i < rows; i++)
+		for(int j = 0; j < columns; j++)
+			(*this)[i][j] = func(i, j);
+}
+
+// Operator[] =========
 TYPE_T
 T* MATRIX<T>::operator[](unsigned int i) const{ return (T*)&(matrix[i*columns]); }
+
+// Operator= (assign) =========
+TYPE_T TYPE_U void MATRIX<T>::operator=(const MATRIX<U>& mat){
+	this->rows = mat.rows;
+	this->columns = mat.columns;
+	this->length = mat.length;
+	for(int i = 0; i < length; i++) this->matrix[i] = mat.matrix[i];
+}
+
+// Operator= (move) ===========
+TYPE_T TYPE_U void MATRIX<T>::operator=(const MATRIX<U>&& mat){
+	this->rows = mat.rows;
+	this->columns = mat.columns;
+	this->length = mat.length;
+	for(int i = 0; i < length; i++) this->matrix[i] = mat.matrix[i];
+}
 
 // Operator+ =========
 TYPE_AB
--- matrixTest.cpp
+++ matrixTest.cpp
@@ -14,13 +14,10 @@ int main(int argv, char** argc){
 
 	MATRIX<int> mat1(3, 4);
 	MATRIX<int> mat2(4, 3);
-	for(int i = 0; i < mat1.getRows(); i++)
-		for(int j = 0; j < mat1.getColumns(); j++) mat1[i][j] = i+j;
-	
-	for(int i = 0; i < mat2.getRows(); i++)
-		for(int j = 0; j < mat2.getColumns(); j++) mat2[i][j] = 5;
+	mat1.fillMatrix([](const int& i, const int& j){return i+j;});
+	mat2.fillMatrix([](const int& i, const int& j){return i*j;});
 
-
+	mat1 = 3*mat1;
 	MATRIX<int> mat3 = mat1*mat2;
 
 	std::cout << "Matrix 1:" << std::endl;
@@ -33,11 +30,5 @@ int main(int argv, char** argc){
 
 	std::cout << "Matrix 1 * Matrix 2:" << std::endl;
 	printMatrix(mat3);
-
-	mat3[2][1] = 8;
-
-	printMatrix(mat3);
-
-	std::cout << "Mat3[2][1] = " << mat3[2][1] << std::endl;
 
 }
