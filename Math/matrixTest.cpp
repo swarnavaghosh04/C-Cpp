dc8a4b52b672a684a37c7bdb2f6cea0ae90b9589
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2020-03-01 16:37:28 -0500

    m

--- Math/matrixTest.cpp
+++ Math/matrixTest.cpp
@@ -14,10 +14,21 @@ void printMatrix(const MAT& m){
 
 int main(int argv, char** argc){
     MAT mat1(3, 4);
-    MAT mat2(3, 4);
+    MAT mat2(4, 4);
+
+    mat1.fill(12);
+    mat2.fill([](m_index i, m_index j){return i+j;});
+
     MAT mat3 = mat1*mat2;
     
+    std::cout << "MATRIX 1:\n";
+    printMatrix(mat1);
+
+    std::cout << "\nMatrix 2:\n";
+    printMatrix(mat2);
+
+    std::cout << "\nMatrix 1 x Matrix 2:\n";
     printMatrix(mat3);
 
-    std::cout << "<=======END=======>" << std::endl;
+    std::cout << "\n<=======END=======>" << std::endl;
 }
