1bd844847ab18d27e79eb2598a1c2319de9a573e
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2020-03-10 10:29:25 -0400

    l

--- Other/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    FILE* f = fopen("C:\\users\\swarn\\OneDrive\\Desktop\\hello.txt", "w+");
-    const char* const myName = "Swarnava Ghosh";
-    fprintf(f, myName);
-    fclose(f);
-    return 0;
-}
