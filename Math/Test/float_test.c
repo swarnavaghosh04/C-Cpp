10200c8c7005553d0b1d62298214fe2af63628d8
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2020-05-17 23:37:55 -0400

    m

---
+++ Math/Test/float_test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+
+    float f = -2;
+
+    return 0;
+}
---
+++ Math/Test/test.s
@@ -0,0 +1,30 @@
+	.file	"float_test.c"
+	.text
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB13:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$16, %esp
+	call	___main
+	flds	LC0
+	fstps	12(%esp)
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE13:
+	.section .rdata,"dr"
+	.align 4
+LC0:
+	.long	-1073741824
+	.ident	"GCC: (MinGW.org GCC Build-20200227-1) 9.2.0"
