53895b7ceb010d5acad287efb98dd1515c0b3665
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2020-05-21 21:54:25 -0400

    c

---
+++ Other/outputSizes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+	printf("char:   %u\n", sizeof(char));
+	printf("short:  %u\n", sizeof(short));
+	printf("int:    %u\n", sizeof(int));
+	printf("long:   %u\n", sizeof(long));
+	printf("float:  %u\n", sizeof(float));
+	printf("double: %u\n", sizeof(double));
+}
